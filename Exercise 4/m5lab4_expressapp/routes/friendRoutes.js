55f16ae1b3890cd028432e55ceeb26e1ca31cbf0
nelg62 <glen.harding.nz@gmail.com>
2024-04-24 10:14:43 +1200

    changed question 3 to find up to question 4

--- Exercise 4/m5lab4_expressapp/routes/friendRoutes.js
+++ Exercise 4/m5lab4_expressapp/routes/friendRoutes.js
@@ -60,53 +60,38 @@ router.get("/info", (req, res) => {
 });
 
 // 3. Dynamic request param endpoint - get the friend matching the specific ID ie. /friends/3
-router.get("/:id", (req, res) => {
-  console.log(req.params);
+router.get('/:id', (req, res) => {
+  console.log(req.params)
   let friendId = req.params.id; // 'id' here will be a value matching anything after the / in the request path
-  let matchingFriends = [...friends];
 
-//   if (friendId) {
-//     matchingFriends = matchingFriends.filter( //try change to find reather then filter 
-//       (friends) => friends.id == friendId
-//     );
-//   }
-
-foundID = matchingFriends.find(friend => friend.id == friendId) 
-
-console.log(foundID)
-
-  if (matchingFriends.length > 0) {
-    res.status(200).json(matchingFriends);
-  } else {
-    res
-      .status(404)
-      .json({ error: "No Person with ID found" + matchingFriends });
-  }
   // Modify this function to find and return the friend matching the given ID, or a 404 if not found
-
+let friendWithID = friends.find(friend => friend.id === parseInt(friendId))
   // Modify this response with the matched friend, or a 404 if not found
-  // res.json({result: 'Finding friend with ID ' + friendId})
-});
+  if (friendWithID) {
+    res.json(friendWithID)
+  }else {
+  res.status(404).json({result: 'Finding friend with ID ' + friendId})
+  }
+})
 
 // a POST request with data sent in the body of the request, representing a new friend to add to our list
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   let newFriend = req.body; // FIRST add this line to index.js: app.use(express.json());
-  console.log(newFriend); // 'body' will now be an object containing data sent via the request body
+  console.log(newFriend) // 'body' will now be an object containing data sent via the request body
 
   // we can add some validation here to make sure the new friend object matches the right pattern
   if (!newFriend.name || !newFriend.gender) {
-    res
-      .status(500)
-      .json({ error: "Friend object must contain a name and gender" });
-    return;
-  } else if (!newFriend.id) {
-    newFriend.id = friends.length + 1; // generate an ID if one is not present
+      res.status(500).json({error: 'Friend object must contain a name and gender'});
+      return;
+  }
+  else if (!newFriend.id) {
+      newFriend.id = friends.length + 1; // generate an ID if one is not present
   }
 
   // if the new friend is valid, add them to the list and return the successfully added object
-  friends.push(newFriend);
-  res.status(200).json(newFriend);
-});
+  friends.push(newFriend)
+  res.status(200).json(newFriend)
+})
 
 // 4. Complete this new route for a PUT request which will update data for an existing friend
 router.put("/:id", (req, res) => {
