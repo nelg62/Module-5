a4a37f8e667c0a9adab1e71de1059d48fcf823dc
nelg62 <glen.harding.nz@gmail.com>
2024-04-26 13:53:41 +1200

    excersize 7 part 1 done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const userRoutes = require('./routes/userRoutes');
 const express = require('express')
-const app = express()
-const port = [3000, 3009, 3007]
+// const app = express()
+// const port = [3000, 3009, 3007]
 // index.js - updated version
 // import the app
 const app = require('./app');
-// const port = 3000
+const port = 3000
 
 const testRoutes =
 require('./routes/myTestRoutes');
@@ -22,20 +22,25 @@ app.use('/', express.static('public'))
 // map the calculator routes to our app
 app.use('/calculator', calculatorRoutes);
 
-port.forEach((item) => {
+// port.forEach((item) => {
 
-app.get('/', (req, res) => {
-res.send('Hello World!')
-})
-app.use('/mytest', testRoutes);
+// app.get('/', (req, res) => {
+// res.send('Hello World!')
+// })
+// app.use('/mytest', testRoutes);
+
+// app.listen(item, () => {
+// console.log(`Example app listening
+// at http://localhost:${item}`)
+// })
+// })
+
 
 // start the app to listen on the right port
 app.listen(port, () => {
     console.log(`Example app listening at
     http://localhost:${port}`)
     })
-})
-
 // node .\index.js
 // npm run start     --created of the above 
 
