46a6c4f58c6b84e86b79662457cde4802b5e730a
nelg62 <glen.harding.nz@gmail.com>
2024-05-04 16:48:14 +1200

    look at bookmarked page thing to get done

--- src/routes/RoutesFakeEcomWebsite.js
+++ src/routes/RoutesFakeEcomWebsite.js
@@ -1,6 +1,8 @@
 const { default: axios } = require("axios");
 const express = require("express");
 const router = express.Router();
+const url = 'https://fakestoreapi.com/products'
+
 
 function getWebsiteData(url) {
     return axios.get(url).then(response => {
